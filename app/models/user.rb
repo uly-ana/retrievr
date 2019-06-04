ca85c08c673cb1a0e20ca920861943e6b8789cb6
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-06-04 14:42:33 +0800

    added routings and user login with username/email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :login
+
   has_many :dogs, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :activity_reviews, dependent: :destroy
@@ -14,4 +16,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
 end
