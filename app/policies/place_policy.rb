ac327234f5ae37ef1cdadf2779a810c13912e8b9
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-06-04 16:30:58 +0800

    added name to place table, finished places controller

---
+++ app/policies/place_policy.rb
@@ -0,0 +1,29 @@
+class PlacePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    user.present?
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    owner?
+  end
+
+  def destroy?
+    owner?
+  end
+
+  private
+
+  def owner?
+    record.user == user
+  end
+end
