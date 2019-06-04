839c06e43da928635ee81a52e043b65b4e965654
Sari Harasti <sariharasti@gmail.com>
2019-06-04 16:24:55 +0800

    Add the controller for activities

--- db/migrate/20190603092500_drop_bookings_table.rb
+++
@@ -1,5 +0,0 @@
-class DropBookingsTable < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :bookings
-  end
-end
---
+++ test/controllers/places_to_go_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PlacesToGoControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
