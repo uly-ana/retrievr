a0d862b7d7a924d564264c56513d0f87c4087d9f
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-06-06 08:55:30 +0800

    further stylized filter box

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,10 @@
 import "bootstrap";
 
 const downFilter = document.querySelector('.dropdown-filter');
+const dropdownContent = document.querySelector('.dropdown-content');
 
 downFilter.addEventListener('click', (e) => {
   e.preventDefault();
 
+  dropdownContent.classList.toggle('dropdown-hide');
 });
