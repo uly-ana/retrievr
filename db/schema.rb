8120009a9e4027a39988c163e5f142f37b7e2c93
Jana Hohlbrugger <jana.hohlbrugger@gmail.com>
2019-06-06 12:19:16 +0800

    added place to favorites table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_094701) do
+ActiveRecord::Schema.define(version: 2019_06_06_040134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,9 @@ ActiveRecord::Schema.define(version: 2019_06_04_094701) do
     t.bigint "activity_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "place_id"
     t.index ["activity_id"], name: "index_favorites_on_activity_id"
+    t.index ["place_id"], name: "index_favorites_on_place_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
@@ -133,6 +135,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_094701) do
   add_foreign_key "activity_reviews", "users"
   add_foreign_key "dogs", "users"
   add_foreign_key "favorites", "activities"
+  add_foreign_key "favorites", "places"
   add_foreign_key "favorites", "users"
   add_foreign_key "guests", "activities"
   add_foreign_key "guests", "users"
