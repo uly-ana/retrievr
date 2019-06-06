5b447a74832726f3c732c0b4824d9e85f6a3b8fb
Rayhan Wirjowerdojo <rayhan.wirjowerdojo@gmail.com>
2019-06-06 22:33:36 +0800

    added inline-svg gem, designed places#show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_040134) do
+ActiveRecord::Schema.define(version: 2019_06_06_104523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2019_06_06_040134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "location"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
