e7c0b0e27e21ffbc31880756d26c15914745fe51
Jana Hohlbrugger <jana.hohlbrugger@gmail.com>
2019-06-11 10:26:55 +0800

    styled profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_062801) do
+ActiveRecord::Schema.define(version: 2019_06_11_022156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_062801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "breed"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
