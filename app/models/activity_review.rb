acd57a1e36006417fa13d77fe23c024c76f11046
Sari Harasti <sariharasti@gmail.com>
2019-06-13 15:12:38 +0800

    activity reviews validation

--- app/models/activity_review.rb
+++ app/models/activity_review.rb
@@ -3,6 +3,6 @@ class ActivityReview < ApplicationRecord
   belongs_to :activity
 
   validates :rating, presence: true, inclusion: { in: 0..5 }
-  validates :content, length: { minimum: 10 }
+  # validates :content, length: { minimum: 10 }
   validates :user_id, presence: true
 end
