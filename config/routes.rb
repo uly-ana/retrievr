e803153a4cba89918edf7e0fd9d514d284c809dd
Jana Hohlbrugger <jana.hohlbrugger@gmail.com>
2019-06-13 15:16:39 +0800

    places details

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
 
   resources :places do
     resources :place_reviews, only: [:create]
+    resources :place_photos, only: [:create]
     resources :favorites, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
